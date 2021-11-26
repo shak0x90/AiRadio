b78b231ee232f3fba0f89085d98d587899642b28
Shakil Ahmed <hridoyhussainshakil@gmail.com>
2021-11-26 23:53:54 +0600

    second commit

---
+++ lib/model/radio.dart
@@ -0,0 +1,13 @@
+class MyRadio {
+  final int id;
+  final int order;
+  final String name;
+  final String tagline;
+  final String color;
+  final String desc;
+  final String url;
+  final String catgor;
+  final String icon;
+  final String img;
+  final String lang;
+}
